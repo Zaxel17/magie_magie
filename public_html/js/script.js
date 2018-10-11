5c87c24bd35e0b101cc80907d6a1463e3dcef5ec
Administrateur <Administrateur@FORMATI-PERG52N>
2018-10-11 10:30:49 +0200

    squellete Html + base de js

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,4 +4,19 @@
  * and open the template in the editor.
  */
 
+function validerInscription(){
+    var s = $('#pseudo').val()
+    alert("Inscription Valider\nBonjour : "+s);
+    document.location.href="listePartie.html";
+}
 
+function creerPartie(){
+    var s = $('#titrePartie').val()
+    alert("Partie Creer !\n"+s);
+    document.location.href="lobby.html";
+}
+
+function lancerPartie(){
+    alert("Partie lancer");
+    //document.location.href="jeux.html";
+}
