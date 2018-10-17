b6c27ad0351cfbd17b252653af58c1b7d90e976c
Administrateur <Administrateur@FORMATI-O0IB14G>
2018-10-17 16:37:31 +0200

    Animation Pioche et jeu du joueur principale, affichage noms des joeurs

--- public_html/js/script-tapis.js
+++ public_html/js/script-tapis.js
@@ -8,16 +8,26 @@ var cartes_joueur = []; /* contient les cartes de chaque joueur */
 var noms_joueur = ['PAULETTE','CRAPAUD','TOTOCHE','DUDULE','BILOUTE']; /* contient les noms de chaque joueur */  
 
     /* pour le test, remplissage pour 5 joueurs */  
-    cartes_joueur[0]=[1,1,1,2,2,2,5,5,5,2]; 
-    cartes_joueur[1]=[1,1,1,5];
-    cartes_joueur[2]=[1,1,1,2,2,5,5,5];
-    cartes_joueur[3]=[1,1,1,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5];
-    cartes_joueur[4]=[1,1,1,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,5,5];
+    cartes_joueur[0]=[1,1,1,2,2,2,2]; 
+    cartes_joueur[1]=[1,1,1,2,2,2,2]; 
+    cartes_joueur[2]=[1,1,1,2,2,2,2]; 
+    cartes_joueur[3]=[1,1,1,2,2,2,2]; 
+    cartes_joueur[4]=[1,1,1,2,2,2,5];
     /* états des 5 sorts disponnible renvoyé aprés analyse du jeu */
     var sortsDisponible = [true,false,true,true,false]; 
     
-var nombre_de_joueurs = 4; /* cartes_joueur.length;  nombre de joueur minimum = 2 */
+var nombre_de_joueurs = 5; /* cartes_joueur.length;  nombre de joueur minimum = 2 */
 
+/* Joueur Adverse */
+if      (nombre_de_joueurs===3) { posLeft = 521; posLeftIncrementation = 521; }
+else if (nombre_de_joueurs===4) { posLeft = 300; posLeftIncrementation = 482; }
+else if (nombre_de_joueurs===5) { posLeft = 160; posLeftIncrementation = 415; }
+else { posLeft = 782; posLeftIncrementation = 0; } // 2 joueurs
+
+
+/* Placement de la pioche */
+jeuHTML += '<div id="pioche" class="taille_carte_normal position_pioche"></div>';
+ 
 /* JOUEUR PRINCIPAL */
 jeuHTML += '<div class="avatar_'+avatars_joueurs[0]+' avatar_joueur"></div><div class="nom_joueur">'+noms_joueur[0]+'</div><ul class="cartes_joueur">';
 for(cj=0;cj<cartes_joueur[0].length;cj++) { jeuHTML += '<li><div class="carte_'+cartes_joueur[0][cj]+' taille_carte_normal"></div></li>'; }
@@ -32,39 +42,56 @@ jeuHTML += '</ul><div class="avatar_'+avatars_joueurs[jadv]+' avatar_joueur_adve
 
 /* AFFICHAGE DES TABLES INITIALE */
 document.getElementById('jeu').innerHTML = jeuHTML; 
- 
+
+/* AVATARS DES JOEURS ADVERSES */
+    poseLeftAv=posLeft;
+for(j=1;j<nombre_de_joueurs;j++) {
+     $('#joueur_'+j).css("left",(poseLeftAv-30)+"px"); /* AVATARS */
+     $('#nom_joueur_'+j).css("left",(poseLeftAv-55)+"px"); /* NOM DU JOUEUR */
+     poseLeftAv = poseLeftAv + posLeftIncrementation;
+}
+        
 // Mise en éventails des cartes */
-function afficherCartesEnEventaille(idHTML,nombre_de_cartes,rayon,radInit,limitRad,topInit,leftInit) {
-    rad = radInit;
-    for(i=0;i<nombre_de_cartes;i++) { 
-        radRotate = rad+1;
-        $('.'+idHTML+' li:eq('+i+')').css("transform","rotate("+radRotate+"rad)");
-        $('.'+idHTML+' li:eq('+i+')').css("-moz-transform","rotate("+radRotate+"rad)");
-        $('.'+idHTML+' li:eq('+i+')').css("-webkit-transform","rotate("+radRotate+"rad)");
-	$('.'+idHTML+' li:eq('+i+')').css("-o-transform","rotate("+radRotate+"rad)");
-        posX = Math.round((Math.cos(rad)*rayon)+leftInit);
-        posY = Math.round((Math.sin(rad)*rayon)+topInit);
-        $('.'+idHTML+' li:eq('+i+')').css("top",posY+"px");
-        $('.'+idHTML+' li:eq('+i+')').css("left",posX+"px");
+function afficherCartesEnEventaille(idHTML,nombre_de_cartes,rayon,radInit,limitRad,leftInit,topInit) {
+    var rad = radInit;
+    
+    function mettreUneCarteEnEventail(i) {
+        if (i > nombre_de_cartes) {
+            return;
+        }
+        var posX = Math.round((Math.cos(rad)*rayon)+leftInit);
+        var posY = Math.round((Math.sin(rad)*rayon)+topInit);      
+        $('.'+idHTML+' li:eq('+i+')').css("transform","translate("+posX+"px,"+posY+"px) rotate("+(rad+1)+"rad)"); 
+        $('.'+idHTML+' li:eq('+i+')').css("-moz-transform","translate("+posX+"px,"+posY+"px) rotate("+(rad+1)+"rad)");
+        $('.'+idHTML+' li:eq('+i+')').css("-webkit-transform","translate("+posX+"px,"+posY+"px) rotate("+(rad+1)+"rad)");
+	$('.'+idHTML+' li:eq('+i+')').css("-o-transform","translate("+posX+"px,"+posY+"px) rotate("+(rad+1)+"rad)");
+        $('.'+idHTML+' li:eq('+i+')').css("transition","all .3s ease");
         rad = rad + (limitRad/nombre_de_cartes);
+        setTimeout(function() {
+            mettreUneCarteEnEventail(i+1);
+        }, 100);
     }
+    mettreUneCarteEnEventail(0);
 }
 
 /* Cartes du Joueur */	
-        afficherCartesEnEventaille('cartes_joueur',cartes_joueur[0].length,600,16.6,1.5,1300,860); 
+afficherCartesEnEventaille('cartes_joueur',cartes_joueur[0].length,600,16.6,1.5,-340,860); 
         
 /* Joueur Adverse */
-if      (nombre_de_joueurs===3) { posLeft = 521; posLeftIncrementation = 521; }
-else if (nombre_de_joueurs===4) { posLeft = 300; posLeftIncrementation = 482; }
-else if (nombre_de_joueurs===5) { posLeft = 160; posLeftIncrementation = 415; }
-else { posLeft = 782; posLeftIncrementation = 0; } // 2 joueurs
+posLeftJA = posLeft-1200-posLeftIncrementation;
+function afficherJeuxJoueursAdverses(j) {
+        if (j > nombre_de_joueurs) {
+            return;
+        }     
+     afficherCartesEnEventaille('joueur_adverse_'+j,cartes_joueur[j].length,130,12.2,4,posLeftJA,-320);
+     posLeftJA+= posLeftIncrementation;
+     setTimeout(function() {
+            afficherJeuxJoueursAdverses(j+1);
+        }, 800);
+    }
+    
+    afficherJeuxJoueursAdverses(0);
 
-for(j=1;j<nombre_de_joueurs;j++) {
-     afficherCartesEnEventaille('joueur_adverse_'+j,cartes_joueur[j].length,130,12.2,4,120,posLeft);
-     $('#joueur_'+j).css("left",(posLeft-30)+"px"); /* AVATARS */
-     $('#nom_joueur_'+j).css("left",(posLeft-55)+"px"); /* NOM DU JOUEUR */
-     posLeft = posLeft + posLeftIncrementation;
- }
 
 /* MASQUES SUR LES COMBINAISONS DE SORT
     0 = sort_invisibilite
